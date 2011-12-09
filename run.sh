22d96b788045eb8951d0aac17a38cca401fe890a
tareq <tareq@tareq-laptop.(none)>
2011-12-09 11:56:10 +0300

    better structuring for the results

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 CUR_DIR=$PWD
-
-OUT_DIR=results
+OUT_BASE=results
+OUT_DIR=$OUT_BASE/results$(date +%Y%m%d_%H_%M_%S)
 
 #PROBLEM_PATH=apps/euler/2d/shockbubble/
 #PROBLEM_NAME=shockbubble
@@ -13,10 +13,12 @@ PROBLEM_NAME=shallow2D
 X_BASE=50
 Y_BASE=50
 
-if [ ! -d $OUT_DIR ]; then
-  mkdir $OUT_DIR
-  echo "Created $OUT_DIR directory to store the results"
+if [ ! -d $OUT_BASE ]; then
+  mkdir $OUT_BASE
+  echo "Created $OUT_BASE directory to store the results directory"
 fi
+mkdir $OUT_DIR
+echo "INFO Created the results direcotry $OUT_DIR"
 
 for WENO_ORDER in 5 #7 9 17
 do
@@ -28,24 +30,24 @@ let NY=$Y_BASE*$SIZE
 
 OUT_NAME=${CUR_DIR}/$OUT_DIR/${PROBLEM_NAME}_${WENO_ORDER}_${NX}x${NY}
 
-echo `date` | tee $OUT_NAME
+echo "INFO" `date` | tee $OUT_NAME
 cd $PROBLEM_PATH
-echo "Entering $PROBLEM_PATH" | tee -a $OUT_NAME
+echo "INFO Entering $PROBLEM_PATH" | tee -a $OUT_NAME
 
 if [ -z "$CPU" ]; then
-	echo "not running on shaheen"
-
+	echo "INFO not running on shaheen"
   COMMAND="python $PROBLEM_NAME.py solver_type=sharpclaw lim_type=2 weno_order=$WENO_ORDER mx=$NX my=$NY"
+
   echo  $COMMAND  | tee -a $OUT_NAME
   $COMMAND 2>&1 | tee -a $OUT_NAME 
 
 else 
-	echo "running on tareq laptop"	
+	echo "INFO running on Shaheen"	
 	# kslrun -t 20:00 -r -m VN -a k47 -n 1 "$BGP_PYTHON shallow2D.py solver_type=sharpclaw mx=100 my=100"
 
 fi 
 cd $CUR_DIR
-echo "Leaving $PROBLEM_PATH" | tee -a $OUT_NAME
+echo "INFO Leaving $PROBLEM_PATH" | tee -a $OUT_NAME
 
 done
 done
